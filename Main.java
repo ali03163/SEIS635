d2a89e9e525ca805e1022fd34bcad72e80811c08
ali03163 <70103631+ali03163@users.noreply.github.com>
2022-04-29 19:56:38 -0500

    Update Main.java
    As a player unfamiliar with the game rules, I want to be able to access a user guide so that I may become comfortable with the game.

--- Main.java
+++ Main.java
@@ -2,10 +2,12 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
+		boolean finished = false;
 		// TODO Auto-generated method stub
+		while(!finished) {
 		Player[] players = new Player[2];
 		Scanner input = new Scanner(System.in);
-		System.out.print("Welcome to Mancala. Select an option: 1- Local Play ");
+		System.out.print("Welcome to Mancala. Select an option: 1- Local Play 2- How to Play ");
 		int selection = input.nextInt();
 		input.nextLine();
 		if(selection == 1) {
@@ -13,7 +15,12 @@ public class Main {
 			players[0] = new Player(input.nextLine());
 			System.out.print("What is the name of the second player? ");
 			players[1] = new Player(input.nextLine());
+			finished = !finished;
+		}
+		else if(selection == 2) {
+			System.out.println("Objective: To collect as many seeds in your store as possible. The player with the most seeds in \r\n"
+					+ "his/her store at the end of the game wins.");
 		}
 	}
-
+	}
 }
