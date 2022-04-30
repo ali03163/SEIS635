94dfcffb11874c3dcc3f6b9e3e3adf9d6589fabf
ali03163 <70103631+ali03163@users.noreply.github.com>
2022-04-29 19:18:17 -0500

    Create Player.java
    As a player, I want to be able to select if I am playing against a computer or local opponent so that I may begin the gameplay against the desired opponent.

---
+++ Player.java
@@ -0,0 +1,16 @@
+
+public class Player {
+	private String name;
+	public Player() {
+		
+	};
+	public Player(String n) {
+		setName(n);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
